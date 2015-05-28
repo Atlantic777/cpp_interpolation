4b9e1102ef4b8d408c3a6a6ee1d875db583ca489
Nikola Hardi <atlantic777@lugons.org>
2015-05-28 17:42:26 +0200

    bilinear interpolation working

--- main.cpp
+++ main.cpp
@@ -50,12 +50,14 @@ uint to_fixed(float f)
 }
 
 
-Mat horizontal(Mat &src, uint dRows, uint dCols, uint fx)
+Mat horizontal(Mat &src, uint dRows, uint dCols, uint fy, uint fx)
 {
     Mat dst = Mat::zeros(dRows, dCols, CV_8UC1);
-    uint target;
-    uint fact;
-    uint iA, iB, res;
+    uint tx, ty;
+    uint fact_x, fact_y;
+    uint iX, iXN, iY, iYN;
+    uint res, rt, rb;
+    uint first, second;
 
     uint one = to_fixed(1);
 
@@ -63,36 +65,70 @@ Mat horizontal(Mat &src, uint dRows, uint dCols, uint fx)
     {
         for(uint col = 0; col < dCols; col++)
         {
-            target = mul(to_fixed(col),fx);
-            iA = target & 0xFFFF0000;
-            iB = iA + one;
-            fact = target & 0xFFFF;
+            tx = mul(to_fixed(col), fx);
+            ty = mul(to_fixed(row), fy);
 
-            if(col > 3 && col < 7 && row == 0)
+            iX = tx & 0xFFFF0000;
+            iXN = iX + one;
+
+            iY = ty & 0xFFFF0000;
+            iYN = iY + one;
+
+            fact_x = tx & 0xFFFF;
+            fact_y = ty & 0xFFFF;
+
+            if(iX >= to_fixed(dCols)) {
+                iX = to_fixed(dCols)-one;
+            }
+
+            if(iXN >= to_fixed(dCols)) {
+                iXN = to_fixed(dCols)-one;
+            }
+
+            if(iY >= to_fixed(dRows)) {
+                iY = to_fixed(dRows)-one;
+            }
+
+            if(iY >= to_fixed(dRows)) {
+                iYN = to_fixed(dRows)-one;
+            }
+
+            int a = src.at<uchar>(iY  >>16, iX  >>16, 0);
+            int b = src.at<uchar>(iY  >>16, iXN >>16, 0);
+            int c = src.at<uchar>(iYN >>16, iX  >>16, 0);
+            int d = src.at<uchar>(iYN >>16, iXN >>16, 0);
+
+            rt = mul((one-fact_x), to_fixed(a))+mul(fact_x,to_fixed(b));
+            rb = mul((one-fact_x), to_fixed(c))+mul(fact_x,to_fixed(d));
+
+            first  = mul((one-fact_y), rt);
+            second = mul(fact_y, rb);
+            res = first + second;
+
+            // res = mul((one-fact_y), to_fixed(rb))+mul(fact_y, to_fixed(rt));
+
+            if(col == -15)
             {
-                cout << "target: "; print_fixed(target);
-                cout << "fact  : "; print_fixed(fact);
-                cout << "iA: "; print_fixed(iA);
-                cout << "iB: "; print_fixed(iB);
+                cout << "tx: "; print_fixed(tx);
+                cout << "ty: "; print_fixed(ty);
                 cout << endl;
+                cout << "fact_x  : "; print_fixed(fact_x);
+                cout << "fact_y  : "; print_fixed(fact_y);
+                cout << endl;
+                cout << "iX: "; print_fixed(iX);
+                cout << "iXN: "; print_fixed(iXN);
+                cout << endl;
+                cout << "iY: "; print_fixed(iY);
+                cout << "iYN: "; print_fixed(iYN);
+                cout << endl;
+                cout << "ABCD vals: " << a << " " << " " << b << " " << c << " " << d << endl;
+                cout << "RT:  "; print_fixed(rt);
+                cout << "RB:  "; print_fixed(rb);
+                cout << "f    "; print_fixed(first);
+                cout << "s    "; print_fixed(second);
+                cout << "Res: "; print_fixed(res);
+                cout << "#####################" << endl;
             }
-
-
-
-            if(iA >= to_fixed(dCols)) {
-                iA = to_fixed(dCols)-one;
-                cout << "fixing: " << col << endl;
-            }
-
-            if(iB >= to_fixed(dCols)) {
-                iB = to_fixed(dCols)-one;
-                cout << "fixing: " << col << endl;
-            }
-
-            int a = src.at<uchar>(row, iA>>16, 0);
-            int b = src.at<uchar>(row, iB>>16, 0);
-
-            res = mul((one-fact), to_fixed(a))+mul(fact,to_fixed(b));
 
             dst.at<uchar>(row, col, 0) = (res&0xFFFF0000)>>16;
         }
@@ -106,11 +142,23 @@ int main(int argc, char** argv )
     Mat image, dst;
     image = imread("compiling.png", CV_LOAD_IMAGE_GRAYSCALE );
 
-    uint dRows = image.rows;
-    uint dCols = 1768;
+    uint dRows = 640;
+    uint dCols = 480;
 
-    float fx = (float)image.cols/dCols;
-    uint  f  = to_fixed(fx);
+    float fx  = (float)image.cols/dCols;
+    uint  ufx = to_fixed(fx);
+
+    float fy  = (float)image.rows/dRows;
+    cout << fy << endl;
+    uint  ufy = to_fixed(fy);
+    print_fixed(ufy);
+    cout << "------------------" << endl;
+
+    uint one = to_fixed(1);
+    print_fixed(one);
+    uint zero = to_fixed(0);
+    print_fixed(zero);
+    print_fixed(one-zero);
 
 
     // uint a = to_fixed(0.5);
@@ -119,8 +167,8 @@ int main(int argc, char** argv )
     // print_fixed(b);
     // print_fixed(mul(a, b));
 
-    dst = horizontal(image, dRows, dCols, f);
-    horizontal(image, dRows, dCols, f);
+    dst = horizontal(image, dRows, dCols, ufy, ufx);
+    // horizontal(image, dRows, dCols, f);
     imshow("cv image", dst);
     while(waitKey(0) != 27);
 
