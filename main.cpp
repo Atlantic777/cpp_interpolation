e762b32beda65a70def251e95ca9bcb44bb96eb1
Nikola Hardi <atlantic777@lugons.org>
2015-05-28 14:05:53 +0200

    fixed point - first results

--- main.cpp
+++ main.cpp
@@ -8,18 +8,35 @@ using namespace std;
 
 const int SCALING_FACTOR = pow(2, 16);
 
+float to_float(uint f)
+{
+    float res;
+    float rh = (f & 0xFFFF0000) / SCALING_FACTOR;
+    float rl = (float)(f & 0xFFFF)/ SCALING_FACTOR;
+    res = rh + rl;
+
+    return res;
+}
+
+void print_fixed(uint a)
+{
+    cout << to_float(a) << endl;
+}
+
 uint mul(uint a, uint b)
 {
-    uint ah = a >> 16;
-    uint bh = b >> 16;
+    unsigned long m = (unsigned long)a*b;
+    uint mh = (m&0xFFFFFFFF00000000) >> 16;
+    uint ml = (m&0xFFFF0000) >> 16;
+    uint res = mh + ml;
 
-    uint al = a & 0xFFFF;
-    uint bl = b & 0xFFFF;
-
-    int h = (ah*bh);
-    int l = (al*bl) >> 16;
-
-    uint res = (h & 0xFFFF0000)  + l;
+    // cout << "beg mul" << endl;
+    // cout << m << endl;
+    // cout << "op1: "; print_fixed(a);
+    // cout << "op2: "; print_fixed(b);
+    // cout << "mh : "; print_fixed(mh);
+    // cout << "ml : "; print_fixed(ml);
+    // cout << "end mul\n" << endl;
 
     return res;
 }
@@ -27,51 +44,57 @@ uint mul(uint a, uint b)
 uint to_fixed(float f)
 {
     uint h = floor(f)*SCALING_FACTOR;
-    uint l = (uint)((f-h)*SCALING_FACTOR);
+    uint l = (uint)((f-floor(f))*SCALING_FACTOR);
 
     return h + l;
 }
 
-float to_float(uint f)
-{
-    float res;
-    float rh = f /SCALING_FACTOR;
-    float rl = (float)(f & 0xFFFF)/SCALING_FACTOR;
-    res = rh + rl;
-    return res;
-}
 
-Mat horizontal(Mat &src, int dRows, int dCols, float fx)
+Mat horizontal(Mat &src, uint dRows, uint dCols, uint fx)
 {
     Mat dst = Mat::zeros(dRows, dCols, CV_8UC1);
-    float target;
-    float fact;
-    int iA, iB, res;
+    uint target;
+    uint fact;
+    uint iA, iB, res;
 
-    for(int row = 0; row < dRows; row++)
+    uint one = to_fixed(1);
+
+    for(uint row = 0; row < dRows; row++)
     {
-        for(int col = 0; col < dCols; col++)
+        for(uint col = 0; col < dCols; col++)
         {
+            target = mul(to_fixed(col),fx);
+            iA = target & 0xFFFF0000;
+            iB = iA + one;
+            fact = iA & 0xFFFF;
 
-            target = col*fx;
-            iA = floor(target);
-            iB = iA + 1;
-            fact = target - iA;
-
-            if(iA >= dCols) {
-                iA = dCols-1;
+            if(col > 3 && col < 7 && row == 0)
+            {
+                cout << "target: ";  print_fixed(target);
+                cout << target << endl << one << endl;
+                cout << "iA: "; print_fixed(iA);
+                cout << "iB: "; print_fixed(iB);
+                cout << endl;
             }
 
-            if(iB >= dCols) {
-                iB = dCols-1;
+
+
+            if(iA >= to_fixed(dCols)) {
+                iA = to_fixed(dCols)-one;
+                cout << "fixing: " << col << endl;
             }
 
-            int a = src.at<uchar>(row, iA, 0);
-            int b = src.at<uchar>(row, iB, 0);
+            if(iB >= to_fixed(dCols)) {
+                iB = to_fixed(dCols)-one;
+                cout << "fixing: " << col << endl;
+            }
 
-            res = (1-fact)*a + fact*b;
+            int a = src.at<uchar>(row, iA>>16, 0);
+            int b = src.at<uchar>(row, iB>>16, 0);
 
-            dst.at<uchar>(row, col, 0) = res;
+            res = mul((one-fact), to_fixed(a))+mul(fact,to_fixed(b));
+
+            dst.at<uchar>(row, col, 0) = (res&0xFFFF0000)>>16;
         }
     }
 
@@ -83,28 +106,23 @@ int main(int argc, char** argv )
     Mat image, dst;
     image = imread("compiling.png", CV_LOAD_IMAGE_GRAYSCALE );
 
-    int dRows = image.rows;
-    int dCols = 800;
-
-    uint f;
+    uint dRows = image.rows;
+    uint dCols = 1768;
 
     float fx = (float)image.cols/dCols;
+    uint  f  = to_fixed(fx);
 
-    f = floor(fx)*SCALING_FACTOR;
-    f = (fx - f)*SCALING_FACTOR;
 
-    float a = 0.5;
-    float b = 0.5;
+    // uint a = to_fixed(0.5);
+    // uint b = to_fixed(2.5);
+    // print_fixed(a);
+    // print_fixed(b);
+    // print_fixed(mul(a, b));
 
-    uint fa = to_fixed(a);
-    uint fb = to_fixed(b);
-
-    uint r  = mul(fa, fb);
-
-    // dst = horizontal(image, dRows, dCols, fx);
-    // imshow("cv image", dst);
-    //
-    // while(waitKey(0) != 27);
+    dst = horizontal(image, dRows, dCols, f);
+    horizontal(image, dRows, dCols, f);
+    imshow("cv image", dst);
+    while(waitKey(0) != 27);
 
     return 0;
 }
